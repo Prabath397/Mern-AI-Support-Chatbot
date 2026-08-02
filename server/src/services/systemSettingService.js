7a3d2bdea90acd022dc8f8597e27bf0ed20abf9b
Prabath Udayanga Jayasuriya <prabathjayasuriya2003@gmail.com>
2026-08-02 13:54:22 +0530

    Reset legacy SupportSphere system prompt

--- server/src/services/systemSettingService.js
+++ server/src/services/systemSettingService.js
@@ -8,7 +8,10 @@ export async function getSystemSetting() {
   let setting = await SystemSetting.findOne().sort({ updatedAt: -1 });
   if (!setting) {
     setting = await SystemSetting.create({});
-  } else if (setting.systemPrompt === LEGACY_SUPPORT_SYSTEM_PROMPT) {
+  } else if (
+    setting.systemPrompt === LEGACY_SUPPORT_SYSTEM_PROMPT ||
+    /support\s*sphere|supportsphere/i.test(setting.systemPrompt)
+  ) {
     setting.systemPrompt = DEFAULT_SYSTEM_PROMPT;
     await setting.save();
   }
